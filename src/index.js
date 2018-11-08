06e52faf64091aa2e9dc7d449c2f827bf5e36163
Swapnil Raj <raj.swapnil99@gmail.com>
2018-11-08 22:04:01 +0000

    Implement summarizing and writing to file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import * as path from 'path';
+import * as fs from 'fs';
 
 import { argParser } from './util/utils';
-import { getName } from './util/dataProcessing';
+import { getName, summarizeRepository } from './util/dataProcessing';
 import { githubClient, GITHUB_USER_QUERY } from './api/api';
 import { makeFolder } from './util/filesystem';
 
@@ -19,5 +20,10 @@ function processRepositories(args, repositories) {
 
 function processRepository(args, repository) {
   const repoDirectory = path.resolve(args.directory, getName(repository));
-  makeFolder(repoDirectory).then(console.log("Yay"));
+  makeFolder(repoDirectory)
+    .then(() => summarizeRepository(repository))
+    .then(data => fs.writeFile(`${repoDirectory}/summary.txt`, data, (err) => {
+      if (err) console.error(err)
+    }))
+    .catch(console.error);
 }

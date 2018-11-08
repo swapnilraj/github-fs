e63a75dd8242a26b538e41ca50b280ee0f1c64d3
Swapnil Raj <raj.swapnil99@gmail.com>
2018-11-08 20:58:05 +0000

    Add utility functions for processing the response from the query

---
+++ src/util/dataProcessing.js
@@ -0,0 +1,27 @@
+/**
+ * Returns a list of collbarotors for a reposiotry.
+ */
+function getCollaborators(repository) {
+  return repository.collaborators.nodes.map(node => node.login);
+}
+
+/**
+ * Returns the description of repo if availbale.
+ */
+function getDescription(repository) {
+  return (repository.description) ? respository.description : '';
+}
+
+/**
+ * Returns the name of repo.
+ */
+export function getName(repository) {
+  return repository.name;
+}
+
+/**
+ * Returns a string summary of the repository object.
+ */
+export function summarizeRepository(repository) {
+
+}
